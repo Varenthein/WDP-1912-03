b308bc43513923af86a6ab1b42a91fc22f1f1268
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2019-12-22 20:54:41 +0100

    Add component HotDealsBox

---
+++ src/components/common/HotDealsBox/HotDealsBox.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './HotDealsBox.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faEye,
+  faStar,
+  faExchangeAlt,
+  faShoppingBasket,
+} from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
+import Button from '../Button/Button';
+
+const HotDealsBox = ({ name, price, oldPrice, promo, stars, image }) => (
+  <div className={styles.root}>
+    <div className={styles.photo}>
+      {promo && <div className={styles.sale}>{promo}</div>}
+      <img className={styles.image} src={image} alt='Ups...'></img>
+      <div className={styles.buttons}>
+        <Button variant='small'>
+          <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
+        </Button>
+      </div>
+    </div>
+    <div className={styles.content}>
+      <h5>{name}</h5>
+      <div className={styles.stars}>
+        {[1, 2, 3, 4, 5].map(i => (
+          <a key={i} href='#'>
+            {i <= stars ? (
+              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+            ) : (
+              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+            )}
+          </a>
+        ))}
+      </div>
+    </div>
+    <div className={styles.line}></div>
+    <div className={styles.actions}>
+      <div className={styles.outlines}>
+        <Button variant='outline'>
+          <FontAwesomeIcon icon={faEye}>Favorite</FontAwesomeIcon>
+        </Button>
+        <Button variant='outline'>
+          <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
+        </Button>
+        <Button variant='outline'>
+          <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
+        </Button>
+      </div>
+      <div className={styles.price}>
+        <Button noHover variant='small'>
+          {oldPrice != 0 && <span className={styles.oldPrice}>$ {oldPrice} </span>}${' '}
+          {price}
+        </Button>
+      </div>
+    </div>
+  </div>
+);
+
+HotDealsBox.propTypes = {
+  children: PropTypes.node,
+  name: PropTypes.string,
+  price: PropTypes.number,
+  promo: PropTypes.string,
+  stars: PropTypes.number,
+  image: PropTypes.string,
+  oldPrice: PropTypes.number,
+};
+
+export default HotDealsBox;
