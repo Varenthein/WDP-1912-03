f0ee11ba40968bab805b46f6c194c3a9737f6da3
Kacper Koza <kacperkoza34@gmail.com>
2020-01-06 00:50:20 +0100

    Finish Gallery component

--- src/components/features/Gallery/Gallery.js
+++ src/components/features/Gallery/Gallery.js
@@ -13,6 +13,8 @@ import {
   faHeadphones,
   faReplyAll,
   faBullhorn,
+  faAngleLeft,
+  faAngleRight,
 } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Gallery.module.scss';
@@ -32,7 +34,7 @@ const Gallery = ({categories, products} ) => {
           </div>
         </div>
         <div className='row'>
-          <div className='col-12 col-md-6 col-lg-6'>
+          <div className='col-md-12 col-lg-6'>
             <div className={styles.cardMenu}>
               <ul>
                 <li>FEATURED</li>
@@ -60,6 +62,60 @@ const Gallery = ({categories, products} ) => {
                   <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon>
                 </div>
               </div>
+              <div className={styles.slider}>
+
+                <div className={styles.btnGlr}>
+                  <FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon>
+                </div>
+
+                <div className={styles.foto}>
+                  <img src={products[0].image}></img>
+                </div>
+                <div className={styles.foto}>
+                  <img src={products[1].image}></img>
+                </div>
+                <div className={styles.foto}>
+                  <img src={products[2].image}></img>
+                </div>
+                <div className={styles.foto}>
+                  <img src={products[3].image}></img>
+                </div>
+                <div className={styles.foto}>
+                  <img src={products[4].image}></img>
+                </div>
+
+                <div className={styles.btnGlr}>
+                  <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
+                </div>
+
+              </div>
+
+              <div className={styles.price}>
+                <div className={styles.text}>
+                  <p>$1200</p>
+                  <p className={styles.oldPrice}>$1200</p>
+                </div>
+              </div>
+
+              <div className={styles.product}>
+              <p>{products[0].name}</p>
+              <div>
+                {[1, 2, 3, 4, 5].map(i => (
+                  <a key={i} href='#'>
+                    {i <= products[0].name.stars ? (
+                      <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+                    ) : (
+                      <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+                    )}
+                  </a>
+                ))}
+              </div>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-12 col-lg-6'>
+            <div className={styles.foto}>
+              <img src={products[7].image} />
             </div>
           </div>
         </div>
